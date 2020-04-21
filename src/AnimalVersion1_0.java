2eeb009fac07c18c67ded112ca389f7d6620c1e6
KnifeofLondon <lzl.dyd@qq.com>
2020-04-21 21:52:23 +0800

    refresh

--- src/AnimalVersion1_0.java
+++ src/AnimalVersion1_0.java
@@ -1,7 +1,7 @@
 import java.awt.datatransfer.StringSelection;
 
 /**
- * java继承、方法重写、final关键字、组合
+ * java继承、方法重写、final关键字、组合、super关键字
  *
  * 1、父类动物
  * 2、远古动物  现代动物
@@ -11,14 +11,15 @@ import java.awt.datatransfer.StringSelection;
  * @version 1.0
  */
 public class AnimalVersion1_0 {
-    public static void main(String[] args){
-        FlyingAnimal eagle = new FlyingAnimal(1000);//不会调用父类的构造函数
+    public static void main(String[] args) {
+        FlyingAnimal eagle = new FlyingAnimal("小鹰","食肉","女",10,3000);//用super调用父类的构造函数
         eagle.fly();
         eagle.eat();
+        //ModernAnimal modernAnimal = new ModernAnimal("小鹰","食肉","女",10);
     }
 }
 
-class Cells{
+class Cells {
     private String function;
 
     public void setFunction(String function) {
@@ -26,74 +27,80 @@ class Cells{
     }
 }
 
-class Animal{
+class Animal {
     private String name;
     private String diets;
     private String sex;
     private int life;
     Cells cells = new Cells();  //组合
 
-    Animal(){};
+    Animal() {
+    }
 
-    Animal(String name , String diets, String sex,int life){
-        this.name=name;
-        this.diets=diets;
-        this.sex=sex;
-        this.life=life;
+    Animal(String name, String diets, String sex, int life) {
+        this.name = name;
+        this.diets = diets;
+        this.sex = sex;
+        this.life = life;
     }
 
 
+    void setName(String name) {
+        this.name = name;
+    }
+    void setDiets(String diets) {
+        this.diets = diets;
+    }
+    void setSex(String sex) {
+        this.sex = sex;
+    }
+    void setLife(int life) {
+        this.life = life;
+    }
 
-    void setName(String name){
-        this.name=name;
-    }
-    void setDiets(String diets){
-        this.diets=diets;
-    }
-    void setSex(String sex){
-        this.sex=sex;
-    }
-    void setLife(int life){
-        this.life=life;
-    }
-
-    void eat(){
+    void eat() {
         System.out.println("进食！");
     }
 
-    void breeding(){
+    void breeding() {
         System.out.println("繁殖！");
     }
 
-    void evolution(){
+    void evolution() {
         System.out.println("进化！");
     }
 }
 
-final class Paleontology extends Animal{  //继承，final关键字，不可继承类
-    void evolution(){
+final class Paleontology extends Animal {  //继承，final关键字，不可继承类
+    void evolution() {
         System.out.println("我们已经灭绝了，不能继续进化了。");
     }
 }
 
-class ModernAnimal extends Animal{
-    void evolution(){  //重写
+class ModernAnimal extends Animal {
+
+    ModernAnimal(String name, String diets, String sex, int life){
+        super(name, diets, sex, life);
+    }
+
+    void evolution() {  //重写
         System.out.println("坚强地活了下来！");
     }
 }
 
-class FlyingAnimal extends ModernAnimal{
+class FlyingAnimal extends ModernAnimal {
+
     private int flyHeight;
-
-    FlyingAnimal(int flyHeight){
-      this.flyHeight = flyHeight;
-    }
-
-    public void setFlyHeight(int flyHeight) {
+    FlyingAnimal(String name, String diets, String sex, int life,int flyHeight) {//通过super调用父类的构造函数
+        super(name, diets, sex, life);
         this.flyHeight = flyHeight;
     }
 
-    void fly(){
+    void setFlyHeight() {
+        this.flyHeight = flyHeight;
+    }
+
+    void fly() {
         System.out.println("我会飞！");
     }
 }
